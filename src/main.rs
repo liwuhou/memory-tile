032b36b9eacd5e90055f24c0a6c0e7b943085753
liwuhou <hugewilliam@foxmail.com>
2023-12-22 07:44:47 +0800

    done

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,64 @@
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    MainWindow::new()?.run()?;
-    Ok(())
+use rand::{seq::SliceRandom, thread_rng};
+use std::rc::Rc;
+
+#[cfg_attr(target_arch = "wasm32", wasm_bindgen::prelude::wasm_bindgen(start))]
+pub fn main() {
+    use slint::Model;
+
+    let main_window = MainWindow::new().unwrap();
+
+    let mut tiles: Vec<TileData> = main_window.get_memory_tiles().iter().collect();
+
+    tiles.extend(tiles.clone());
+
+    let mut rng = thread_rng();
+    tiles.shuffle(&mut rng);
+
+    let tiles_model = Rc::new(slint::VecModel::from(tiles));
+    main_window.set_memory_tiles(tiles_model.clone().into());
+
+    let main_window_weak = main_window.as_weak();
+
+    main_window.on_check_if_pair_solved(move || {
+        let mut flipped_tiles = tiles_model
+            .iter()
+            .enumerate()
+            .filter(|(_, tile)| tile.image_visible && !tile.solved);
+
+        if let (Some((t1_idx, mut t1)), Some((t2_idx, mut t2))) =
+            (flipped_tiles.next(), flipped_tiles.next())
+        {
+            let is_pair_solved = t1 == t2;
+            if is_pair_solved {
+                t1.solved = true;
+                tiles_model.set_row_data(t1_idx, t1);
+                t2.solved = true;
+                tiles_model.set_row_data(t2_idx, t2);
+            } else {
+                let main_window = main_window_weak.unwrap();
+                main_window.set_disabled_tiles(true);
+                let tiles_model = tiles_model.clone();
+                slint::Timer::single_shot(std::time::Duration::from_secs(1), move || {
+                    main_window.set_disabled_tiles(false);
+                    t1.image_visible = false;
+                    tiles_model.set_row_data(t1_idx, t1);
+                    t2.image_visible = false;
+                    tiles_model.set_row_data(t2_idx, t2);
+                })
+            }
+        }
+    });
+
+    main_window.run().unwrap();
 }
 
 slint::slint! {
+    struct TileData {
+        image: image,
+        image_visible: bool,
+        solved: bool,
+    }
+
     component MemoryTile inherits Rectangle {
         callback clicked;
         in property <bool> open_curtain;
@@ -12,6 +67,9 @@ slint::slint! {
 
         height: 64px;
         width: 64px;
+        border-radius: 8px;
+        clip: true;
+
         background: solved ? #34CE57 : #3960D5;
         animate background { duration: 800ms; }
 
@@ -37,7 +95,7 @@ slint::slint! {
             width: open_curtain ? 0px : (parent.width / 2);
             height: parent.height;
             animate width { duration: 250ms; easing: ease-in; }
-            // animate x { duration: 250ms; easing: ease-in; }
+            animate x { duration: 250ms; easing: ease-in; }
         }
 
         TouchArea {
@@ -49,11 +107,36 @@ slint::slint! {
     }
 
     export component MainWindow inherits Window {
-        MemoryTile {
-            icon: @image-url("icons/bus.png");
-            solved: true;
+        width: 326px;
+        height: 326px;
+
+        callback check_if_pair_solved();
+        in property <bool> disabled_tiles;
+
+        in-out property <[TileData]> memory_tiles: [
+            { image: @image-url("icons/at.png") },
+            { image: @image-url("icons/balance-scale.png") },
+            { image: @image-url("icons/bicycle.png") },
+            { image: @image-url("icons/bus.png") },
+            { image: @image-url("icons/cloud.png") },
+            { image: @image-url("icons/cogs.png") },
+            { image: @image-url("icons/motorcycle.png") },
+            { image: @image-url("icons/video.png") },
+        ];
+
+        for tile[i] in memory_tiles : MemoryTile {
+            x: mod(i, 4) * 74px;
+            y: floor(i / 4) * 74px;
+            width: 64px;
+            height: 64px;
+            icon: tile.image;
+            open_curtain: tile.image_visible || tile.solved;
+            solved: tile.solved;
             clicked => {
-                self.open_curtain = !self.open_curtain;
+                if (!root.disabled_tiles) {
+                    tile.image_visible = !tile.image_visible;
+                    root.check_if_pair_solved();
+                }
             }
         }
     }
